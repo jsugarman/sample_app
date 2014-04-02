aec3038e0d1dbcd18df77596e0e523fd0970ba4a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2014-04-02 12:57:05 +0100

    upto tutorial section 4.6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	#function to return the full title of page 
+	def full_title(page_title_in)
+		base_title = "Ruby on Rails Tutorial Sample App"
+
+		if page_title_in.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title_in}"
+		end
+	end
+
 end
