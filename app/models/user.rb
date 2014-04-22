0769c2757aaf3c8708e340b117c2eb7303e7270f
jsugarman <joel.sugarman@digital.justice.gov.uk>
2014-04-22 23:24:33 +0100

    upto section 6.4 - conclusion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
-	validates(:name, presence: true)
-	validates(:email, presence: true)
+	
+	before_save{ self.email = email.downcase }
+
+	validates 	:name,
+				presence: true,
+				length: { maximum: 64 }
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  	validates 	:email,
+				presence: true,
+				format: { with: VALID_EMAIL_REGEX },
+				uniqueness: {case_sensitive: false} 
+
+	validates	:password,
+				length: { minimum: 6}
+
+	has_secure_password
+
 end
---
+++ db/migrate/20140416172732_users_email_chk.rb
@@ -0,0 +1,5 @@
+class UsersEmailChk < ActiveRecord::Migration
+  def change
+  	#needs doing - look up check constraints in rails db migration classes
+  end
+end
