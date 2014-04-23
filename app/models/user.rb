d1de56ab4168fa4dcdccd073a05b91ecbb1971a2
jsugarman <joel.sugarman@digital.justice.gov.uk>
2014-04-23 21:52:24 +0100

    pre-signup chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
-	
-	before_save{ self.email = email.downcase }
+
+	#bcrypt-ruby Gem add-in method
+	has_secure_password
+
+	#before insert,update,delete triggers
+	before_save{ email.downcase! }
 
 	validates 	:name,
 				presence: true,
 				length: { maximum: 64 }
 	
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   	validates 	:email,
 				presence: true,
@@ -16,6 +20,6 @@ class User < ActiveRecord::Base
 	validates	:password,
 				length: { minimum: 6}
 
-	has_secure_password
+	
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,17 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when email is mixed case" do
+  	let(:mixed_case_email) { "foO@bAR.Com" }
+
+  	it "should be saved as lower case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase 
+  	end
+
+  end
+
   describe "when name is too long" do
     before { @user.name = "a" * 65 }
     it { should_not be_valid }
@@ -36,7 +47,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
