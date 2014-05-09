59ba228baf3e5aca5f242c67bae28a467fbd8166
jsugarman <joel.sugarman@digital.justice.gov.uk>
2014-05-09 09:27:36 +0100

    upto section 9,9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
-  
+ 
+ # -----------------------
 	  def show
 	  	@user = User.find(params[:id])
 	  end
-
+# -----------------------
 	  def new
 	  	@user = User.new
 	  end
+# -----------------------
+	  def edit
+		@user = User.find(params[:id])
+	  end
+# -----------------------
+	  def update
+	    @user = User.find(params[:id])
+	    if @user.update_attributes(user_params)
+	      # Handle a successful update
+	      # @user.save
+	      flash[:success] = "Profile updated"
+		  redirect_to @user
 
+	      # flash[:success] = "Details Updated Succesfully!"
+	      # redirect_to @user
+	    else
+	      render 'edit'
+	    end
+	  end
+# -----------------------
 	  def create
 	    @user = User.new(user_params)    # Not the final implementation!
 	    if @user.save
@@ -18,13 +38,14 @@ class UsersController < ApplicationController
 			render 'new'
 	    end
 	  end
-
+# -----------------------
   private
 
+# -----------------------
 	  def user_params
 	  		#specify required params and those permitted, all others restricted to prevent csrf
 	  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
-
+# -----------------------
   
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,8 @@ describe "User Pages" do
 
   subject { page }
 
-
-  describe "profile page" do
+# -----------------------------
+  describe "profile" do
 	  
 	  # Replace with code to make a user variable
 	  #needs amending to user factorygirl gem when wifi available
@@ -13,23 +13,22 @@ describe "User Pages" do
 	  let (:user) { FactoryGirl.create(:user) }
 	  before { visit user_path(user) }
 
-	  it { should have_content(user.name) }
-	  it { should have_title(user.name) }
-  end
-
-
-  describe "signup page" do
-
-    before { visit signup_path }
-
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    describe "page" do
+	   it { should have_content(user.name) }
+	   it { should have_title(user.name) }
+    end
 
   end
 
+# -----------------------------
   describe "signup" do
 
     before { visit signup_path }
+
+    describe "page" do
+      it { should have_content('Sign up') }
+      it { should have_title(full_title('Sign up')) }
+    end
 
     let(:submit) { "Create my account" }
 
@@ -44,18 +43,11 @@ describe "User Pages" do
       	it { should have_title('Sign up') }
       	it { should have_content('error') }
         it { should have_error_message('error')}
-      	# it { should have_selector('div.alert.alert-error') }
       end
-
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { valid_signup }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -68,12 +60,47 @@ describe "User Pages" do
         it { should have_link('Sign out') }
       	it { should have_title(user.name) }
         it { should have_success_message("Welcome") }
-      	# it { should have_selector('div.alert.alert-success', text: "Welcome") }
-
       end
-
-
+    end
+  end
+# -----------------------------
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in(user)
+      visit edit_user_path(user)
     end
 
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_error_message('error')}
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
   end
+  # -----------------------------
+
 end		
