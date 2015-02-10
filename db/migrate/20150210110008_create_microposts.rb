8f5cbc0fb2c1af950173fdc1d510c9416f6ea386
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-02-10 13:02:05 +0000

    tidied up test table removal

--- db/migrate/20150210110008_create_microposts.rb
+++ db/migrate/20150210110008_create_microposts.rb
@@ -2,7 +2,7 @@ class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
       t.string :content
-      t.integer :user_id
+      t.integer :user_idx
 
       t.timestamps
     end
